f078a7ac5838b786156412501eab0cde35565225
ryohei-ochi-fr <ryohei.ochi@futurerays.biz>
2025-05-22 15:04:40 +0900

    copilotレビューの反映

--- src/SVNBrowser.java
+++ src/SVNBrowser.java
@@ -1,67 +1,52 @@
-
 import org.tmatesoft.svn.core.*;
 import org.tmatesoft.svn.core.auth.ISVNAuthenticationManager;
 import org.tmatesoft.svn.core.io.SVNRepository;
 import org.tmatesoft.svn.core.io.SVNRepositoryFactory;
 import org.tmatesoft.svn.core.wc.SVNWCUtil;
 
-import java.text.SimpleDateFormat;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.Collection;
-import java.util.Iterator;
 
 public class SVNBrowser {
-	
-	private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy/MM/dd,HH:mm");
+
+    private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("yyyy/MM/dd,HH:mm").withZone(ZoneId.systemDefault());
+    private static final String OUTPUT_HEADER = "filename,yyyy/mm/dd,hh:mm,filesize,revision";
 
     private static void listEntries(SVNRepository repository, String path) throws SVNException {
-        Collection entries = repository.getDir(path, -1, null, (Collection) null);
-        Iterator iterator = entries.iterator();
-        
-        
-        while (iterator.hasNext()) {
-        	
-        	// ファイル名
-        	// コミット時のタイムスタンプ　年月日と時刻に分ける
-        	// ファイルサイズ(バイト)
-        	// リビジョン
-        	
-            SVNDirEntry entry = (SVNDirEntry) iterator.next();
-            
-            String formattedDate = DATE_FORMAT.format(entry.getDate());
-            System.out.println(entry.getName() + "," + formattedDate + "," + entry.getRevision());
+        Collection<SVNDirEntry> entries = repository.getDir(path, -1, null, (Collection<?>) null);
+        for (SVNDirEntry entry : entries) {
+            String formattedDate = DATE_FORMAT.format(entry.getDate().toInstant());
+            System.out.println(entry.getName() + "," + formattedDate + "," + entry.getSize() + "," + entry.getRevision());
 
             if (entry.getKind() == SVNNodeKind.DIR) {
-                listEntries(repository, (path.equals("")) ? entry.getName() : path + "/" + entry.getName());
+                listEntries(repository, (path.isEmpty()) ? entry.getName() : path + "/" + entry.getName());
             }
         }
     }
 
     public static void main(String[] args) {
-
         if (args.length != 3) {
-            System.out.println("usage: SVNBrowser username password svn:url");
+            System.err.println("Error: Missing arguments. Usage: SVNBrowser <username> <password> <svn:url>");
             System.exit(1);
         }
 
         String name = args[0];
         String password = args[1];
         String url = args[2];
-        
-        System.out.println("filename,yyyy/mm/dd,hh:mm,filesize,revision");
-        
+
         try {
             SVNRepository repository = SVNRepositoryFactory.create(SVNURL.parseURIEncoded(url));
             ISVNAuthenticationManager authManager = SVNWCUtil.createDefaultAuthenticationManager(name, password);
             repository.setAuthenticationManager(authManager);
 
             System.out.println("Repository Root: " + repository.getRepositoryRoot(true));
-            // getUUID() の呼び出しを削除
+            System.out.println(OUTPUT_HEADER);
 
             listEntries(repository, "");
 
         } catch (SVNException e) {
             System.err.println("Error while accessing the repository: " + e.getMessage());
-            e.printStackTrace();
         }
     }
 }
